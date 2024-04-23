2a900f545434f548866f6fee5d048263ef587961
Joris Schelfhout <joris@shelfwood.co>
2024-04-23 15:49:04 +0200

    Add package.json, tailwind.config.js, and shopping-cart.svg

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#2563EB",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
